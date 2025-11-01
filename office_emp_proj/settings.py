0f92604bb115b991f998dfb045f215b00aa7e273
Shahbaz Alam <sbzalam2024@gmail.com>
2025-11-02 01:47:44 +0530

    STATICFILES_STORAGE = 'whitenoise.storage.ManifestStaticFilesStorage'

--- office_emp_proj/settings.py
+++ office_emp_proj/settings.py
@@ -132,7 +132,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Use WhiteNoise's storage backend in production
 if not DEBUG:
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.ManifestStaticFilesStorage'
 
 
 # Default primary key field type
