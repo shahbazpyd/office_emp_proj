7378a8cb30d52cab43c2b49f785371f831135fb4
Shahbaz Alam <sbzalam2024@gmail.com>
2025-11-02 11:45:33 +0530

    delete build.sh and update vercel.json

--- build.sh
+++
@@ -1,9 +0,0 @@
-# In build.sh (Alternative - Try this if the first one doesn't work)
-#!/bin/bash
-
-# 1. Install dependencies (ensure they are installed in a location the shell can find)
-pip install -r requirements.txt
-
-# 2. Use the shell's python
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
