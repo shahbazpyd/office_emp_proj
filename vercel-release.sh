1c22bb1a5725d1447a9925eb1c8a04f0f6d836e9
Shahbaz Alam <sbzalam2024@gmail.com>
2025-11-02 19:13:26 +0530

    logs for migration

--- vercel-release.sh
+++ vercel-release.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+echo "--- VERCEL RELEASE: START ---"
+echo "Running Django database migrations..."
 python manage.py migrate --noinput
+echo "Django migrations finished."
+echo "--- VERCEL RELEASE: END ---"
