a7518c2e2d9f4c72e3f02170c970893bb7d57426
Shahbaz Alam <sbzalam2024@gmail.com>
2025-11-02 16:52:54 +0530

    new changed for production

--- office_emp_proj/wsgi.py
+++ office_emp_proj/wsgi.py
@@ -2,16 +2,10 @@
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise 
-from django.conf import settings # 💡 NEW: Import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'office_emp_proj.settings')
 
 application = get_wsgi_application()
-
-# 💡 FIX: Wrap with WhiteNoise, explicitly setting the root to STATIC_ROOT
-# WhiteNoise will now look exactly where collectstatic put the files.
-application = WhiteNoise(application, root=settings.STATIC_ROOT)
 
 # Vercel needs this 'app' variable
 app = application
